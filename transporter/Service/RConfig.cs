f2203d7d2554c35bc286d5b9a2dd592a65eb6d21
Илья Кротких <ivashca99@gmail.com>
2017-10-13 23:25:01 +0300

    Improved data transmission and integrity.

--- transporter/Service/RConfig.cs
+++ transporter/Service/RConfig.cs
@@ -50,35 +50,58 @@ namespace Transporter.Service
 
         public RConfig(bool isSource,string sourceIP , string destinationIP)
         {
-            int messageSPort;   // Source message port
-            int messageDPort;   // Destination message port
-            int dataSPort;      // Source data port
-            int dataDPort;      // Destination data port
-            IPAddress SIP;      // Source IP
-            IPAddress DIP;      // Destination IP
+            //int messageSPort;   // Source message port
+            //int messageDPort;   // Destination message port
+            //int dataSPort;      // Source data port
+            //int dataDPort;      // Destination data port
+            //IPAddress SIP;      // Source IP
+            //IPAddress DIP;      // Destination IP
 
-            SIP = IPAddress.Parse(sourceIP);
-            DIP = IPAddress.Parse(destinationIP);
+            //SIP = IPAddress.Parse(sourceIP);
+            //DIP = IPAddress.Parse(destinationIP);
 
-            if (isSource)
-            {
-                messageSPort = 8080;
-                messageDPort = 8081;
-                dataSPort = 8082;
-                dataDPort = 8083;
-            }
-            else
-            {
-                messageSPort = 8081;
-                messageDPort = 8080;
-                dataSPort = 8083;
-                dataDPort = 8082;
-            }
+            //if (isSource)
+            //{
+            //    messageSPort = 8080;
+            //    messageDPort = 8081;
+            //    dataSPort = 8082;
+            //    dataDPort = 8083;
+            //}
+            //else
+            //{
+            //    messageSPort = 8081;
+            //    messageDPort = 8080;
+            //    dataSPort = 8083;
+            //    dataDPort = 8082;
+            //}
 
-            messageSEndPoint = new IPEndPoint(SIP, messageSPort);
-            messageDEndPoint = new IPEndPoint(DIP, messageDPort);
-            dataSEndPoint = new IPEndPoint(SIP, dataSPort);
-            dataDEndPoint = new IPEndPoint(DIP, dataDPort);
+            //messageSEndPoint = new IPEndPoint(SIP, messageSPort);
+            //messageDEndPoint = new IPEndPoint(DIP, messageDPort);
+            //dataSEndPoint = new IPEndPoint(SIP, dataSPort);
+            //dataDEndPoint = new IPEndPoint(DIP, dataDPort);
+
+            int messagePort = 8080;
+            int dataPort = 8082;
+            IPAddress SIP = IPAddress.Parse(sourceIP);
+            IPAddress DIP = IPAddress.Parse(destinationIP);
+
+            messageSEndPoint = new IPEndPoint(SIP, messagePort);
+            messageDEndPoint = new IPEndPoint(DIP, messagePort);
+            dataSEndPoint = new IPEndPoint(SIP, dataPort);
+            dataDEndPoint = new IPEndPoint(DIP, dataPort);
+        }
+
+        public RConfig(bool isSource, IPAddress sourceIP, IPAddress destinationIP)
+        {
+            int messagePort = 8080;
+            int dataPort = 8082;
+            IPAddress SIP = sourceIP;
+            IPAddress DIP = destinationIP;
+
+            messageSEndPoint = new IPEndPoint(SIP, messagePort);
+            messageDEndPoint = new IPEndPoint(DIP, messagePort);
+            dataSEndPoint = new IPEndPoint(SIP, dataPort);
+            dataDEndPoint = new IPEndPoint(DIP, dataPort);
         }
     }
 }
--- transporter/Transporter.cs
+++ transporter/Transporter.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Transporter.Service;
 
@@ -9,8 +11,13 @@ namespace Transporter
 {
     public class Transporter
     {
-        public Client transporterClient;
-        public RConfig transporterConfig;
+        private List<byte[]> listDataBlocks;
+        private bool transporterIsFree;
+        private bool sendObjectIsFree;
+        private CancellationTokenSource ctsWaitWork;
+
+        public Client transporterClient { get; private set; }
+        public RConfig transporterConfig { get; private set; }
 
         public event DataEventHandler onSClientGetData = delegate { };
         public event EventHandler onSClientDataListenerCreated = delegate { };
@@ -21,7 +28,6 @@ namespace Transporter
         public event EventHandler onDClientCancel = delegate { };
         public event EventHandler<Exception> onSClientError = delegate { };
 
-        private List<byte[]> listDataBlocks;
 
         public Transporter(bool isMaster)
         {
@@ -33,7 +39,15 @@ namespace Transporter
 
         public Transporter(bool isMaster, string sourceClientIP, string destinationClientIP)
         {
-            transporterConfig = new RConfig(isMaster,sourceClientIP, destinationClientIP);
+            transporterConfig = new RConfig(isMaster, sourceClientIP, destinationClientIP);
+            transporterClient = new Client(transporterConfig);
+
+            SetEvents();
+        }
+
+        public Transporter(bool isMaster, IPAddress sourceClientIP, IPAddress destinationClientIP)
+        {
+            transporterConfig = new RConfig(isMaster, sourceClientIP, destinationClientIP);
             transporterClient = new Client(transporterConfig);
 
             SetEvents();
@@ -51,13 +65,22 @@ namespace Transporter
             transporterClient.onMessageListenerClosed += transporterClient_onMessageListenerClosed;
         }
 
+        public void SetConfig(RConfig config)
+        {
+            this.transporterConfig = config;
+            transporterClient.config = config;
+        }
+
         public void StartService()
         {
             transporterClient.StartListeningMesages();
+            transporterIsFree = true;
+            sendObjectIsFree = true;
         }
 
         public void StopService()
         {
+            transporterIsFree = false;
             transporterClient.SendMessage(new Message() { messageCommands = MessageCommands.CloseMessageListener }, transporterConfig.messageSEndPoint);
         }
 
@@ -66,37 +89,73 @@ namespace Transporter
             Metadata objectMetadata = new Metadata();
             try
             {
+                if (transporterIsFree != true || sendObjectIsFree != true)
+                    throw new Exception("Impossible at the moment");
+                sendObjectIsFree = false;
+
                 byte[] dataMass = transporterClient.ObjectToByteArray(obj);
                 listDataBlocks = DevideByteMass(ref objectMetadata, dataMass);
                 transporterClient.onGetDataListenerCreated += SendObject_onDataListenerCreated;
                 transporterClient.onGetDataListenerCreated += transporterClient_onGetDataListenerCreated;
                 transporterClient.SendMessage(new Message { messageCommands = MessageCommands.OpenDataListener, metadata = objectMetadata });
+                WaitAnswer(20);
+
             }
             catch (Exception ex)
             {
+                transporterClient.onGetDataListenerCreated -= SendObject_onDataListenerCreated;
+                transporterClient.onGetDataListenerCreated -= transporterClient_onGetDataListenerCreated;
+
                 Console.WriteLine(ex);
+                onSClientError(this, ex);
+                sendObjectIsFree = true;
             }
         }
 
         private void SendObject_onDataListenerCreated(object sender, EventArgs e)
         {
-            try
+            if (ctsWaitWork != null)
+                ctsWaitWork.Cancel();
+
+            transporterIsFree = false;
+            Task sendTask = new Task(() =>
             {
-                int recivedBlocks = 0;
-                do
+                try
                 {
-                    transporterClient.SendData(listDataBlocks[recivedBlocks++]);
-                } while (recivedBlocks < listDataBlocks.Count);
-            }
-            catch (Exception ex)
+                    int recivedBlocks = 0;
+                    do
+                    {
+                        transporterClient.SendData(listDataBlocks[recivedBlocks++]);
+                    } while (recivedBlocks < listDataBlocks.Count);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    transporterClient.onGetDataListenerCreated -= SendObject_onDataListenerCreated;
+                    transporterClient.onGetDataListenerCreated -= transporterClient_onGetDataListenerCreated;
+                    transporterIsFree = true;
+                }
+            });
+            sendTask.Start();
+        }
+
+        private void WaitAnswer(int seconds)
+        {
+            ctsWaitWork = new CancellationTokenSource();
+
+            Task sleepTask = new Task(() => 
             {
-                Console.WriteLine(ex);
-            }
-            finally
-            {
-                transporterClient.onGetDataListenerCreated -= SendObject_onDataListenerCreated;
-                transporterClient.onGetDataListenerCreated -= transporterClient_onGetDataListenerCreated;
-            }
+                Thread.Sleep(seconds * 1000);
+
+                ctsWaitWork.Token.ThrowIfCancellationRequested();
+
+                onSClientError(this, new Exception("The answer was not received within the allotted time."));
+                this.sendObjectIsFree = true;
+            }, ctsWaitWork.Token);
+            sleepTask.Start();
         }
 
         // I dont know how it make better.
